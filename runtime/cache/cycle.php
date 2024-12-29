0dc572ae8389c616ba2a7dd453b00c89fe1412f1
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-29 17:49:48 +0330

    .

---
+++ runtime/cache/cycle.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'user' => 
+  array (
+    1 => 'App\\Domain\\Entity\\User',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'App\\Domain\\Repository\\UserRepository',
+    5 => 'default',
+    6 => 'users',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'email' => 'email',
+      'mobile' => 'mobile',
+      'password' => 'password',
+      'roles' => 'roles',
+      'otpCode' => 'otp_code',
+      'otpExpiredAt' => 'otp_expired_at',
+    ),
+    10 => 
+    array (
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'int',
+      'email' => 'string',
+      'mobile' => 'string',
+      'password' => 'string',
+      'roles' => 'string',
+      'otpCode' => 'string',
+      'otpExpiredAt' => 'datetime',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
+);
