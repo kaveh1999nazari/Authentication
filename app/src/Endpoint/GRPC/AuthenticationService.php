161bd6319cb754402334e57da90d713b3e427e9e
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-29 17:49:35 +0330

    add authentication service

---
+++ app/src/Endpoint/GRPC/AuthenticationService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Endpoint\GRPC;
+
+use App\Domain\Entity\User;
+use Cycle\ORM\ORMInterface;
+use GRPC\authentication\AuthenticationUserGrpcInterface;
+use GRPC\authentication\LoginEmailRequest;
+use GRPC\authentication\LoginEmailResponse;
+use GRPC\authentication\LoginMobileRequest;
+use GRPC\authentication\LoginMobileResponse;
+use Spiral\Auth\TokenStorageInterface;
+use Spiral\RoadRunner\GRPC;
+
+
+class AuthenticationService implements AuthenticationUserGrpcInterface
+{
+    public function __construct(private readonly ORMInterface $orm, private readonly TokenStorageInterface $token)
+    {
+    }
+
+    public function LoginByMobile(GRPC\ContextInterface $ctx, LoginMobileRequest $in): LoginMobileResponse
+    {
+        $user = $this->orm->getRepository(User::class)
+            ->findByMobile($in->getMobile());
+
+        $response = new LoginMobileResponse();
+
+        if ($user && password_verify($in->getPassword(), $user->getPassword())) {
+
+            $token = $this->token->create(['sub' => $user->getId()]);
+
+            $response->setToken($token->getID());
+            $response->setMessage($user->getRoles());
+
+
+            return $response;
+        } else {
+            $response->setMessage(["Authentication failed."]);
+
+        }
+
+        return $response;
+
+    }
+
+    public function LoginByEmail(GRPC\ContextInterface $ctx, LoginEmailRequest $in): LoginEmailResponse
+    {
+        $user = $this->orm->getRepository(User::class)
+            ->findOne(['email' => $in->getEmail()]);
+
+        $response = new LoginEmailResponse();
+
+        if ($user &&
+            $in->getCode() === $user->getOtpCode() &&
+            $user->getOtpExpiredAt() > new \DateTimeImmutable() ) {
+
+            $token = $this->token->create(['sub' => $user->getId()]);
+
+            $response->setMessage($user->getRoles());
+            $response->setToken($token->getID());
+
+        } else {
+            $response->setMessage(["Authentication failed."]);
+        }
+
+        return $response;
+    }
+
+}
