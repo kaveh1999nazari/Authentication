631f04323a5f537219745594272cd5f70694ced9
kaveh1999nazari <kaveh1999nazari@gmail.com>
2024-12-30 09:34:53 +0330

    .

--- runtime/cache/cycle.php
+++ runtime/cache/cycle.php
@@ -48,4 +48,49 @@
       'id' => 2,
     ),
   ),
+  'token' => 
+  array (
+    1 => 'Spiral\\Cycle\\Auth\\Token',
+    2 => 'Cycle\\ORM\\Mapper\\Mapper',
+    3 => 'Cycle\\ORM\\Select\\Source',
+    4 => 'Cycle\\ORM\\Select\\Repository',
+    5 => 'default',
+    6 => 'auth_tokens',
+    7 => 
+    array (
+      0 => 'id',
+    ),
+    8 => 
+    array (
+      0 => 'id',
+    ),
+    9 => 
+    array (
+      'id' => 'id',
+      'hashedValue' => 'hashed_value',
+      'createdAt' => 'created_at',
+      'expiresAt' => 'expires_at',
+      'payload' => 'payload',
+    ),
+    10 => 
+    array (
+    ),
+    12 => NULL,
+    13 => 
+    array (
+      'id' => 'string',
+      'hashedValue' => 'string',
+      'createdAt' => 'datetime',
+      'expiresAt' => 'datetime',
+      'payload' => 'string',
+    ),
+    14 => 
+    array (
+    ),
+    19 => NULL,
+    20 => 
+    array (
+      'id' => 2,
+    ),
+  ),
 );
